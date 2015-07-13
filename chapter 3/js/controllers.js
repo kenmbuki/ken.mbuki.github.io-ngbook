c67e8deefb0457a0e03e67d3f992cf6fc9eaec93
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2015-07-13 10:49:00 +0300

    Card Generator

---
+++ chapter 3/js/controllers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('myApp', [])
+.controller('CardController', function($scope){
+  $scope.user = {
+    name: 'Ken Mbuki',
+    occupation: 'White Hat',
+    email: 'ken.mbuki@savannahinformatics.com',
+    link1: 'savannahinformatics.com',
+    tusername:'Ken_Mbuki',
+    companylogo: 'http://savannahinformatics.com/wp-content/uploads/2014/08/savannah-informatics-logo1.jpg',
+    color1: '#c0c0c0',
+    color2: 'white',
+    textcolor1: '#287cc2',
+    textcolor2: '#666',
+    fusername:'mbuki',
+    gusername:'Ken Mbuki'
+  }
+});
